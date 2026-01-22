f04af84288ec5f2647b766f7168fc6f161269cb2
Luca Taschner <Admin@NinjaGaming.gg>
2026-01-22 17:16:20 +0100

    Moved `GuiHelper` to own object and implement GUI logic for server selection with dynamic item assignment based on server status.

---
+++ src/main/kotlin/gg/ninjagaming/lobbyswitcher/misc/GuiHelper.kt
@@ -0,0 +1,49 @@
+package gg.ninjagaming.lobbyswitcher.misc
+
+import gg.ninjagaming.lobbyswitcher.LobbySwitcher
+import gg.ninjagaming.lobbyswitcher.LobbySwitcher.Companion.cfg
+import gg.ninjagaming.lobbyswitcher.LobbySwitcher.Companion.currentServer
+import gg.ninjagaming.lobbyswitcher.LobbySwitcher.Companion.servers
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+
+object GuiHelper {
+    fun openGui(player: Player) {
+        val inventory: Inventory = Bukkit.createInventory(
+            null, cfg.getInt("inventory.rows") * 9,
+            ChatColor.translateAlternateColorCodes(
+                '&',
+                cfg.getString("inventory.title")!!
+            )
+        )
+
+        if (currentServer == null) LobbySwitcher.getServer(player)
+
+        for (servers in servers.values) {
+            if (!servers.isOnline) {
+                //Offline Server
+                val offlineItem = ServerItemBuilder.buildOfflineServerItem(servers)
+
+                inventory.setItem(servers.slot, offlineItem)
+                continue
+
+            }
+            // Current Server
+            if (servers.serverName == currentServer) {
+                val current = ServerItemBuilder.buildCurrentServerItem(servers)
+                inventory.setItem(servers.slot, current)
+                continue
+            }
+
+            //Online Server
+            val online = ServerItemBuilder.buildOnlineServerItem(servers)
+            inventory.setItem(servers.slot, online)
+            // < ONLINE
+
+        }
+
+        player.openInventory(inventory)
+    }
+}
